229feadb8e57e216c802e79aa19b6856fa977422
Hollow1238 <currietyler45@gmail.com>
2023-12-11 00:03:52 +1100

    Updated Arithmetic Formatter.py
    Created 'get_equation_elements' to find the first and second numbers in the equation, including the operator.
    Rewrote 'calculations' function to use 'get_equation_elements' instead of running through the loop each time in its function.
    Changed 'main' to include the new calculations loop

--- Arithmetic Formatter.py
+++ Arithmetic Formatter.py
@@ -2,47 +2,68 @@ import math
 
 list = ["32 + 698",'673 - 127']
 
-acceptable = {'0','1','2','3','4','5','6','7','8','9','+','-',' '}
+def get_equation_elements(equation_string):
+
+    first_number = int(equation_string[:equation_string.index(' ')])
+
+    second_number = int(equation_string[equation_string.index(' ', equation_string.index(' ') + 1 ):])
+
+    operator = equation_string[equation_string.index(' ') + 1]
+
+    return first_number , second_number , operator
+
 def correct_input(problems):
+    acceptable = {'0','1','2','3','4','5','6','7','8','9','+','-',' '}
+
     if len(problems) > 5:
         print("Error: Too many problems.")
+        quit()
+
     for problem in problems:
-        op_index = problem.index(' ') + 1
-        operator = problem[op_index]
+        operator_index = problem.index(' ') + 1
+        operator = problem[operator_index]
+
         if operator not in acceptable:
             print('Error: Operator must be ''+'' or ''-''.')
+            quit()
+
         for element in problem:
+
             if element not in acceptable:
                 print('Error: Numbers must only contain digits.')
-        number1 = problem[:op_index - 1]
-        number2 = problem[op_index + 2:]
+                quit()
+
+        number1 = problem[:operator_index - 1]
+        number2 = problem[operator_index + 2:]
+
         if len(number1) > 4 or len(number2) > 4:
             print('Error: Numbers cannot be more than four digits.')
+            quit()
+        
 
-def calculations(problems):
-    for problem in problems:
-        op_index = problem.index(' ') + 1
-        operator = problem[op_index]
-        number1 = int(problem[:op_index - 1])
-        number2 = int(problem[op_index + 2:])
-        if operator == '-':
-            sum = number1 - number2
-        else:
-            sum = number1 + number2
-        return sum
 
+
+def calculations(problem):
+    elements_of_equation = get_equation_elements(problem)
+    if elements_of_equation[2] == '+':
+       sum = int(elements_of_equation[0]) + int(elements_of_equation[1])
+    else:
+       sum = int(elements_of_equation[0]) - int(elements_of_equation[1])
+    return sum
+        
+        
 
 
 #def formatting():
 
 
 
-#def main(list):
-    correct_input(list)
+def main(problems):
+    correct_input(problems)
+    for problem in problems:
+        calculations(problem)
 
 
 
 #main(list)
 
-print(calculations(list))
-how does github work
